58d32b80b20b7b3e15ec1e333e6a77f0095ecb5e
blance714 <blance_714@outlook.com>
2025-07-26 14:04:04 +0800

    fix(gateway): resolve vpn_portal loopback connection failure

--- easytier/src/gateway/tcp_proxy.rs
+++ easytier/src/gateway/tcp_proxy.rs
@@ -708,9 +708,10 @@ impl<C: NatDstConnector> TcpProxy<C> {
             tracing::warn!("set_nodelay failed, ignore it: {:?}", e);
         }
 
-        let nat_dst = if Some(nat_entry.real_dst.ip())
-            == global_ctx.get_ipv4().map(|ip| IpAddr::V4(ip.address()))
-        {
+        let is_loopback = Some(nat_entry.real_dst.ip())
+            == global_ctx.get_ipv4().map(|ip| IpAddr::V4(ip.address()));
+
+        let nat_dst = if is_loopback {
             format!("127.0.0.1:{}", nat_entry.real_dst.port())
                 .parse()
                 .unwrap()
@@ -718,7 +719,11 @@ impl<C: NatDstConnector> TcpProxy<C> {
             nat_entry.real_dst
         };
 
-        let _guard = global_ctx.net_ns.guard();
+        let _guard = if !is_loopback {
+            Some(global_ctx.net_ns.guard())
+        } else {
+            None
+        };
         let Ok(dst_tcp_stream) = connector.connect(nat_entry.src, nat_dst).await else {
             tracing::error!("connect to dst failed: {:?}", nat_entry);
             nat_entry.state.store(NatDstEntryState::Closed);
